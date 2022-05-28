34c55a428eefaa58a1b414577919829108264684
shojin-pro <shojin.debu@gmail.com>
2022-05-28 21:13:47 +0900

    change profile-images, names.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -22,12 +22,12 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-      <Image
+{/*       <Image
         src="/images/profile.jpg" // Route of the image file
         height={144} // Desired size with correct aspect ratio
         width={144} // Desired size with correct aspect ratio
         alt="Your Name"
-       />
+       /> */}
     </Layout>
   );
 }
